874101d275ad2bbd5a9da0cb94fe339a90713a72
enuesaa <nsrciog@gmail.com>
2023-09-10 16:07:27 +0900

    feat: db migration

--- go.mod
+++ go.mod
@@ -11,7 +11,11 @@ require (
 require (
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	gorm.io/driver/mysql v1.5.1 // indirect
+	gorm.io/gorm v1.25.4 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"database/sql"
 	"log"
 	"os"
+	"time"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -16,13 +17,44 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	db, err := sql.Open("mysql", os.Getenv("DB_DSN"))
+	dsn := os.Getenv("DB_DSN")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
-	defer db.Close()
-	if err := db.Ping(); err != nil {
-		log.Fatalf("failed to ping: %v", err)
-	}
 	log.Println("Successfully connected to PlanetScale!")
+    rows, err := db.Raw("show tables").Rows()
+	if err != nil {
+		log.Fatalf("failed to run: %v", err)
+	}
+	defer rows.Close()
+
+	fmt.Printf("%+v", rows)
+
+	type Note struct {
+		ID           uint    `gorm:"primaryKey"`
+		Name         string  `gorm:"type:varchar(255)"`
+		Content      string  `gorm:"type:text"`
+		Comment      string  `gorm:"type:text"`
+		CreatedAt    time.Time `gorm:"type:timestamp;not null;default:current_timestamp"`
+		UpdatedAt    time.Time `gorm:"type:timestamp;not null;default:current_timestamp on update current_timestamp"`
+	}
+	// db.Migrator().CreateTable(&Note{})
+	// db.Migrator().DropTable(&Note{})
+	if err := db.AutoMigrate(&Note{}); err != nil {
+		fmt.Printf("failed %s", err)
+	}
+
+	type Tag struct {
+		ID           uint    `gorm:"primaryKey"`
+		NoteId       uint    `gorm:"not null"`
+		Name         string  `gorm:"type:varchar(255)"`
+		Value        string  `gorm:"type:varchar(255)"`
+		CreatedAt    time.Time `gorm:"type:timestamp;not null;default:current_timestamp"`
+		UpdatedAt    time.Time `gorm:"type:timestamp;not null;default:current_timestamp on update current_timestamp"`
+	}
+	// db.Migrator().DropTable(&Tag{})
+	if err := db.AutoMigrate(&Tag{}); err != nil {
+		fmt.Printf("failed %s", err)
+	}
 }
