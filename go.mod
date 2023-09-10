6ff7a4953b3e86b9a6a1cc352c37c710e4eee0fd
enuesaa <nsrciog@gmail.com>
2023-09-10 18:35:22 +0900

    feat: create config repository

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- main.go
+++ main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	// "log"
+	// "os"
 
 	"github.com/enuesaa/pinit/pkg/repository"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	repo := repository.ConfigRepository{}
+	config, _ := repo.ReadConfig()
+	fmt.Printf("%+v", config)
 
-	env := repository.Env {
-		DATABASE_DSN: os.Getenv("DATABASE_DSN"),
-	}
-	repos := repository.NewRepos(env)
-	fmt.Printf("%+v\n", repos)
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+
+	// env := repository.Env {
+	// 	DATABASE_DSN: os.Getenv("DATABASE_DSN"),
+	// }
+	// repos := repository.NewRepos(env)
+	// fmt.Printf("%+v\n", repos)
 }
