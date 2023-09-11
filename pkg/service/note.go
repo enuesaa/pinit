80222368932760ce4398c9924dbab4eaf2388de0
enuesaa <nsrciog@gmail.com>
2023-09-11 23:56:46 +0900

    feat: create service skelton

--- pkg/service/note.go
+++ pkg/service/note.go
@@ -12,3 +12,25 @@ type Note struct {
 	CreatedAt time.Time `gorm:"type:timestamp;not null;default:current_timestamp"`
 	UpdatedAt time.Time `gorm:"type:timestamp;not null;default:current_timestamp on update current_timestamp"`
 }
+
+type NoteService struct {}
+
+func (srv *NoteService) list() []Note {
+	return make([]Note, 0)
+}
+
+func (srv *NoteService) get() (Note, error) {
+	return *new(Note), nil
+}
+
+func (srv *NoteService) create() error {
+	return nil
+}
+
+func (srv *NoteService) update() error {
+	return nil
+}
+
+func (srv *NoteService) remove() error {
+	return nil
+}
--- pkg/service/tag.go
+++ pkg/service/tag.go
@@ -12,3 +12,26 @@ type Tag struct {
 	CreatedAt time.Time `gorm:"type:timestamp;not null;default:current_timestamp"`
 	UpdatedAt time.Time `gorm:"type:timestamp;not null;default:current_timestamp on update current_timestamp"`
 }
+
+
+type TagService struct {}
+
+func (srv *TagService) list() []Tag {
+	return make([]Tag, 0)
+}
+
+func (srv *TagService) get() (Tag, error) {
+	return *new(Tag), nil
+}
+
+func (srv *TagService) create() error {
+	return nil
+}
+
+func (srv *TagService) update() error {
+	return nil
+}
+
+func (srv *TagService) remove() error {
+	return nil
+}
