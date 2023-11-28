aa148d39e062bc22b8da14d68e7dd3b2621b015f
nua <enuesaa@gmail.com>
2023-11-29 00:35:18 +0900

    feat: customize cli help message template

--- main.go
+++ main.go
@@ -8,24 +8,41 @@ import (
 )
 
 func main() {
-	cmd := &cobra.Command{
+	app := &cobra.Command{
 		Use:     "pinit",
 		Short:   "A personal note-taking app",
 		Version: "0.0.3",
 	}
 
 	repos := repository.NewRepos()
-	cmd.AddCommand(cli.CreateConfigureCmd(repos))
-	cmd.AddCommand(cli.CreateLsCmd(repos))
-	cmd.AddCommand(cli.CreateCreateCmd(repos))
-	cmd.AddCommand(cli.CreateRmCmd(repos))
-	cmd.AddCommand(cli.CreateUpCmd(repos))
+	app.AddCommand(cli.CreateConfigureCmd(repos))
+	app.AddCommand(cli.CreateLsCmd(repos))
+	app.AddCommand(cli.CreateCreateCmd(repos))
+	app.AddCommand(cli.CreateRmCmd(repos))
+	app.AddCommand(cli.CreateUpCmd(repos))
 	usecase.Init(repos)
 
 	// disable default
-	cmd.SetHelpCommand(&cobra.Command{Hidden: true})
-	cmd.PersistentFlags().BoolP("help", "", false, "Show help information")
-	cmd.CompletionOptions.DisableDefaultCmd = true
+	app.SetHelpCommand(&cobra.Command{Hidden: true})
+	app.CompletionOptions.DisableDefaultCmd = true
+	app.SilenceUsage = true
+	app.PersistentFlags().SortFlags = false
+	app.PersistentFlags().BoolP("help", "", false, "Show help information")
+	app.PersistentFlags().BoolP("version", "", false, "Show version")
+	app.SetHelpTemplate(`{{.Short}}
+{{if .HasAvailableSubCommands}}{{$cmds := .Commands}}{{if eq (len .Groups) 0}}
+Available Commands:{{range $cmds}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{else}}{{range $group := .Groups}}
 
-	cmd.Execute()
+{{.Title}}{{range $cmds}}{{if (and (eq .GroupID $group.ID) (or .IsAvailableCommand (eq .Name "help")))}}
+{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{end}}
+{{end}}
+{{if .HasAvailableLocalFlags}}Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+{{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}
+`)
+
+	app.Execute()
 }
