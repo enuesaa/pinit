56aa802ab77ee50732251684e590af02e530b0d9
nua <enuesaa@gmail.com>
2023-12-03 19:43:05 +0900

    feat(web): react query で setQueryData と言うメソッドを呼べば mock データを差し込めるらしい

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -5,6 +5,11 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
+  // see https://oita.oika.me/2021/09/19/react-query-stub
+  // mock
+  queryClient.setQueryData('getConfig', {
+    token: 'aa',
+  })
 
   return (
     <QueryClientProvider client={queryClient}>
