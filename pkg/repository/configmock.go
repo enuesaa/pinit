6889562cde682013fb0209b40f8af796264d6cd1
nua <enuesaa@gmail.com>
2023-12-10 16:22:18 +0900

    refactor: ConfigMock can use Config internally because viper can handle default value of configuration instead of load config file.

--- pkg/repository/configmock.go
+++ pkg/repository/configmock.go
@@ -1,53 +1,13 @@
 package repository
 
 type ConfigMockRepository struct {
-	items map[string]string
+	ConfigRepository
 }
 
 func (repo *ConfigMockRepository) Init() {}
 
 func (repo *ConfigMockRepository) Load() error {
 	return nil
-}
-
-func (repo *ConfigMockRepository) DbHost() string {
-	return repo.items["dbhost"]
-}
-
-func (repo *ConfigMockRepository) SetDbHost(dbhost string) {
-	repo.items["dbhost"] = dbhost
-}
-
-func (repo *ConfigMockRepository) DbUsername() string {
-	return repo.items["dbusername"]
-}
-
-func (repo *ConfigMockRepository) SetDbUsername(dbusername string) {
-	repo.items["dbusername"] = dbusername
-}
-
-func (repo *ConfigMockRepository) DbPassword() string {
-	return repo.items["dbpassword"]
-}
-
-func (repo *ConfigMockRepository) SetDbPassword(dbpassword string) {
-	repo.items["dbpassword"] = dbpassword
-}
-
-func (repo *ConfigMockRepository) DbName() string {
-	return repo.items["dbname"]
-}
-
-func (repo *ConfigMockRepository) SetDbName(dbname string) {
-	repo.items["dbname"] = dbname
-}
-
-func (repo *ConfigMockRepository) ChatgptToken() string {
-	return repo.items["chatgpttoken"]
-}
-
-func (repo *ConfigMockRepository) SetChatgptToken(chatgpttoken string) {
-	repo.items["chatgpttoken"] = chatgpttoken
 }
 
 func (repo *ConfigMockRepository) Write() error {
