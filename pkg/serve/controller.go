288b648d5954f0b599146c59cf12bcfe57758f34
nua <enuesaa@gmail.com>
2023-12-16 16:17:02 +0900

    fix(serve): show binders

--- pkg/serve/controller.go
+++ pkg/serve/controller.go
@@ -1,6 +1,8 @@
 package serve
 
 import (
+	"fmt"
+
 	"github.com/enuesaa/pinit/pkg/repository"
 	"github.com/enuesaa/pinit/pkg/service"
 	"github.com/enuesaa/pinit/pkg/usecase"
@@ -11,9 +13,34 @@ type Controller struct {
 	Repos repository.Repos
 }
 
+type ListBindersResponseItemBinder struct {
+	Id         string `json:"id"`
+	Name       string `json:"name"`
+	Category   string `json:"category"`
+	ArchivedAt string `json:"archivedAt"`
+	CreatedAt  string `json:"createdAt"`
+	UpdatedAt  string `json:"updatedAt"`
+}
+type ListBindersResponse struct {
+	Items []ListBindersResponseItemBinder `json:"items"`
+}
+
 func (ctl *Controller) ListBinders(c *fiber.Ctx) error {
 	binders := usecase.ListBinders(ctl.Repos)
-	return c.JSON(binders)
+	res := ListBindersResponse{
+		Items: make([]ListBindersResponseItemBinder, 0),
+	}
+	for _, binder := range binders {
+		res.Items = append(res.Items, ListBindersResponseItemBinder{
+			Id: fmt.Sprintf("%d", binder.ID),
+			Name: binder.Name,
+			Category: binder.Category,
+			ArchivedAt: "",
+			CreatedAt: binder.CreatedAt.String(),
+			UpdatedAt: binder.UpdatedAt.String(),
+		})
+	}
+	return c.JSON(res)
 }
 
 func (ctl *Controller) ListNotes(c *fiber.Ctx) error {
