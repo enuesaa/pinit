9e51da2c8da766eaa247a8df44df146ae954418c
nua <enuesaa@gmail.com>
2023-12-24 19:54:56 +0900

    feat: try to select data with ent

--- pkg/cli/pocent.go
+++ pkg/cli/pocent.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/enuesaa/pinit/ent"
+	"github.com/enuesaa/pinit/ent/binder"
 	"github.com/enuesaa/pinit/pkg/repository"
 	"github.com/spf13/cobra"
 )
@@ -14,7 +15,9 @@ func CreatePocentsCmd(repos repository.Repos) *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:   "pocent",
 		Short: "pocent",
+		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			name := args[0]
 			fmt.Printf("poc ent command\n")
 
 			dsn := repos.Database.Dsn()
@@ -24,11 +27,22 @@ func CreatePocentsCmd(repos repository.Repos) *cobra.Command {
 			}
 			defer client.Close()
 
-			binder, err := client.Binder.Create().SetName("hello").SetCategory("test").Save(context.Background())
+			binderA, err := client.Binder.Create().
+				SetName(name).
+				SetCategory("test").
+				Save(context.Background())
 			if err != nil {
 				log.Fatalf("Error: failed to create binder\n%s\n", err.Error())
 			}
-			fmt.Printf("%+v\n", binder)
+			fmt.Printf("created: %+v\n", binderA)
+
+			binderB, err := client.Binder.Query().
+				Where(binder.NameEQ(name)).
+				Only(context.Background())
+			if err != nil {
+				log.Fatalf("Error: failed to get binder\n%s\n", err.Error())
+			}
+			fmt.Printf("fetched: %+v\n", binderB)
 		},
 	}
 
