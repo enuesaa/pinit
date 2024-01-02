f67f9c3935d0a2d9bda7cca1e325ad2c7c2f5a40
nua <enuesaa@gmail.com>
2024-01-02 16:14:31 +0900

    fix: run query `select id from binder limit 1` when opening database connection because ent.Open seems not to care its connection (selectable)

--- pkg/usecase/database.go
+++ pkg/usecase/database.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/enuesaa/pinit/pkg/repository"
@@ -8,14 +9,18 @@ import (
 
 func OpenDbConnection(repos repository.Repos) error {
 	if err := repos.Database.Open(); err != nil {
-		return fmt.Errorf("failed to open db connetcion.\n%s\n", err.Error())
+		return fmt.Errorf("failed to open db connetcion.")
+	}
+	_, err := repos.Database.Binder().Query().Select("id").Limit(1).All(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to open db connetcion.")
 	}
 	return nil
 }
 
 func CloseDbConnection(repos repository.Repos) error {
 	if err := repos.Database.Close(); err != nil {
-		return fmt.Errorf("failed to close db connection.\n%s\n", err.Error())
+		return fmt.Errorf("failed to close db connection.")
 	}
 	return nil
 }
