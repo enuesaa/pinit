fd9f7509ab282635ab3632551b20b1fc95716029
nua <enuesaa@gmail.com>
2024-01-13 19:53:37 +0900

    chore(ci): use sqlite3

--- main_test.go
+++ main_test.go
@@ -3,15 +3,17 @@ package main
 import (
 	"log"
 	"testing"
-	// "github.com/enuesaa/pinit/pkg/repository"
+
+	"github.com/enuesaa/pinit/pkg/repository"
+	"github.com/enuesaa/pinit/pkg/usecase"
 )
 
 func TestMain(m *testing.M) {
-	// repos := repository.NewTestRepos()
+	repos := repository.NewRepos()
 
-	// if err := usecase.Migrate(repos); err != nil {
-	// 	log.Fatalf("Error: failed to migrate, %s", err.Error())
-	// }
+	if err := usecase.CreateRegistryIfNotExist(repos); err != nil {
+		log.Fatalf("Error: failed to migrate, %s", err.Error())
+	}
 
 	code := m.Run()
 	log.Printf("test finished with code %d", code)
