b399d583ddc1e8a47894d7ef78586d1db50b0c9c
nua <enuesaa@gmail.com>
2024-01-17 02:43:32 +0900

    chore: remove playground menu because no longer needed.

--- web/src/components/common/Header.tsx
+++ web/src/components/common/Header.tsx
@@ -1,21 +1,15 @@
-import { Box, Container, Flex, Link, Separator } from '@radix-ui/themes'
+import { Container, Flex, Link, Separator } from '@radix-ui/themes'
 import { FaBolt } from 'react-icons/fa'
 import styles from './Header.css'
-import { PlaygorundMenu } from './PlaygorundMenu'
 
 export const Header = () => {
   return (
     <header className={styles.main}>
       <Container size='4' pt='1' pr='5' pb='2'>
-        <Flex width='100%' height='7'>
-          <Link href='/' className={styles.heading}>
-            <FaBolt />
-            pinit
-          </Link>
-          <Box grow='1' style={{ textAlign: 'right' }}>
-            <PlaygorundMenu />
-          </Box>
-        </Flex>
+        <Link href='/' className={styles.heading}>
+          <FaBolt />
+          pinit
+        </Link>
       </Container>
       <Separator size='4' />
     </header>
